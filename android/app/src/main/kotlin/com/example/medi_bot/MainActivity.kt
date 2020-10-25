433a6a8853dcd45e13b86ad2cdbe04bf28aabccd
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-10-25 11:23:57 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/medi_bot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medi_bot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/MainScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("MediBot"),),
+      body: Column(children: [
+
+      ],),
+    );
+  }
+}
