8261fcb127970456867e6dbfcef60f82cdbfbe24
Gebes <mail@gebes.eu>
2020-10-31 22:06:06 +0100

    Fixed theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:medi_bot/screens/main_screen.dart';
@@ -8,15 +9,16 @@ void main() {
 }
 
 class MediBotApp extends StatelessWidget {
-
   static final navigatorKey = new GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     return PlatformApp(
       title: 'MediBot',
       home: MainScreen(),
       navigatorKey: navigatorKey,
-      material: (_,__)=> MaterialAppData(theme: ThemeData(primaryColor: Colors.green, primarySwatch: Colors.green)),
+      material: (_, __) => MaterialAppData(theme: ThemeData(primaryColor: Colors.green, primarySwatch: Colors.green), darkTheme: ThemeData.light()),
+      cupertino: (_,__ )=> CupertinoAppData(theme: CupertinoThemeData(brightness: Brightness.light)),
       onGenerateRoute: (RouteSettings settings) {
         final args = settings.arguments;
         switch (settings.name) {
@@ -34,4 +36,3 @@ class MediBotApp extends StatelessWidget {
     );
   }
 }
-
