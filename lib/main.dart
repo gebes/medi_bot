be2a4125bccc3c2b8b6e922f4d092ff72d00e95d
Laurens K <39766854+laurensk@users.noreply.github.com>
2021-01-17 12:57:33 +0100

    ios app runnable

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:medi_bot/screens/main_screen.dart';
 import 'package:page_transition/page_transition.dart';
 
@@ -13,17 +12,18 @@ class MediBotApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PlatformApp(
+    return MaterialApp(
       title: 'MediBot',
       home: MainScreen(),
       navigatorKey: navigatorKey,
-      material: (_, __) => MaterialAppData(theme: ThemeData(primaryColor: Colors.green, primarySwatch: Colors.green), darkTheme: ThemeData(primaryColor: Colors.green, primarySwatch: Colors.green, brightness: Brightness.light)),
-      cupertino: (_,__ )=> CupertinoAppData(theme: CupertinoThemeData(brightness: Brightness.light, primaryColor: CupertinoColors.systemGreen)),
+      theme: ThemeData(primaryColor: Colors.green, primarySwatch: Colors.green),
+      darkTheme: ThemeData(primaryColor: Colors.green, primarySwatch: Colors.green, brightness: Brightness.light),
       onGenerateRoute: (RouteSettings settings) {
         final args = settings.arguments;
         switch (settings.name) {
           case '/home':
-            return PageTransition(type: PageTransitionType.fade, child: MainScreen(), duration: Duration(milliseconds: 500));
+            return PageTransition(
+                type: PageTransitionType.fade, child: MainScreen(), duration: Duration(milliseconds: 500));
 
           default:
             return MaterialPageRoute(
